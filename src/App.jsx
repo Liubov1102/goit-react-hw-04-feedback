d21daa92a51fe62bf4e724658d2358474f127bf5
Liubov1102 <laav.odessa@gmail.com>
2022-07-17 19:07:00 +0300

    hw-4-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,66 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { FeedbackOptions } from "./components/FeedbackOptions/FeedbackOptions";
 import { Section } from "./components/Section/Section";
 import { Statistics } from "./components/Statistics/Statistics";
 import { Notification } from "./components/Notification/Notification";
 
-export class App extends Component  {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+export const App = () => {
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
 
-  onLeaveFeedback = (e) => {
-    const name = e.target.name;
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1
-    }));
-  };
+	const leaveFeedback = option => {
+		switch (option) {
+			case 'good':
+				setGood(state => state + 1);
+				break;
+			case 'neutral':
+				setNeutral(state => state + 1);
+				break;
+			case 'bad':
+				setBad(state => state + 1);
+				break;
+			default:
+				break;
+		}
+	};
 
-  countTotalFeedback = () => {
-    return (this.state.good + this.state.neutral + this.state.bad);
-  };
+	const countTotalFeedback = () => {
+		return good + neutral + bad;
+	};
+	
+	const total = countTotalFeedback();
 
-  countPositiveFeedbackPercentage = () => {
-    return (Math.round(this.state.good / this.countTotalFeedback() * 100 ));
-  };
+	const countPositiveFeedbackPercentage = () => {
+		return (Math.round( good / total * 100));
+	};
 
-  render() {
-		const { good, neutral, bad } = this.state;
-		const total = this.countTotalFeedback();
-		const positivePercentage = this.countPositiveFeedbackPercentage();
+	const positivePercentage = countPositiveFeedbackPercentage();
+		
+	return (
+		<>
+			<Section title="Please leave feedback">
+				<FeedbackOptions
+					options={['good', 'neutral', 'bad']}
+					onLeaveFeedback={leaveFeedback} />
+			</Section>
 
-		const objKey = Object.keys(this.state);
-		return (
-			<>
-				<Section title="Please leave feedback">
-					<FeedbackOptions options={objKey} onLeaveFeedback={this.onLeaveFeedback} />
+			{total === 0 ? (
+				<Notification>
+					There is no feedback
+				</Notification>
+			) : (
+				<Section title="Statistics">
+					<Statistics
+						good={good}
+						neutral={neutral}
+						bad={bad}
+						total={total}
+						positivePercentage={positivePercentage}
+					/>
 				</Section>
+			)}
+		</>
+	);
+};
 
-				{total === 0 ? (
-					<Notification> There is no feedback
-						</Notification>
-				) : (
-					<Section title="Statistics">
-						<Statistics
-							good={good}
-							neutral={neutral}
-							bad={bad}
-							total={total}
-							positivePercentage={positivePercentage}
-						/>
-					</Section>
-				)}
-			</>
-		);
-	}
-}
--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -11,8 +11,12 @@ export const Button = styled.button`
     cursor: pointer;
     font-weight: 500;
     font-size: 16px;
+    transition: box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
 :hover {
-    background: rgb(175, 175, 175);
+    color: #fff;
+    background-color: #188ce8;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 
 `;
